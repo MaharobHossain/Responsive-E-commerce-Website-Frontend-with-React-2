73dde5d315b5c1640a234ab3e1666eda1204c5e4
MaharobHossain <maharobhossain8880@gmail.com>
2024-02-19 13:38:54 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+
+const Home = () => {
+    
+    return (
+        <>
+              
+              <p> Hello,This is Kodeoo Limited.</p>
+              
+        </>
+    );
+}
+
+export default Home;
