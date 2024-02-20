e96268ad50ac3b477c553476f9e7b8a81b82bf09
MaharobHossain <maharobhossain8880@gmail.com>
2024-02-20 10:05:17 +0600

    Navbar Add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-
-
+// import Navbar from './Components/Navbar/Navbar';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Shop from './Pages/Shop';
+// import ShopCategory from './Pages/ShopCategory';
+// import Product from './Pages/Product';
+// import LoginSignup from './Pages/LoginSignup';
+// import Cart from './Pages/Cart';
+// import Footer from './Components/Footer/Footer';
+// import men_banner from './Components/Assetss/banner_mens.png'
+// import women_banner from './Components/Assetss/banner_women.png'
+// import kid_banner from './Components/Assetss/banner_kids.png'
+// import Login from './Pages/Login';
 
 function App() {
   return (
     <div>
-      
+    {/* <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    
+    <Route exact path='/' element={<Shop/>} />
+    <Route exact path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+    <Route exact path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+    <Route exact path="/kids" element={<ShopCategory banner={kid_banner} category="kid"/>} />
+    <Route exact path='/product' element={<Product/>}>
+    <Route exact path=':ProductId' element={<Product/>} />
+    </Route>
+    <Route exact path='/cart'  element={<Cart/>} />
+    <Route exact path='/login'  element={<Login/>} />
+    <Route exact path='/singup'  element={<LoginSignup/>} />
 
 
-
-
-      <Routes>
-      <Route exact path="/" element={<Home />} />
     </Routes>
+    <Footer/>
+    </BrowserRouter> */}
+    
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useState } from 'react'
+import './Navbar.css'
+import logo from '../Assest/Images/logo.png'
+import cart from '../Assest/Images/Cart.png'
+import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
+
+
+const Navbar = () => {
+
+      const [menu, setMenu]=useState("shop");
+      const {getTotalCartItems}= useContext(ShopContext);
+
+  return (
+    <div className='navbar'>
+
+      {/* <Link to='/'>    
+        
+      </Link> */}
+      <div className="nav-logo">
+           <img src={logo} alt="" />
+          <p >BRO'S  COLLECTION</p>
+      </div>
+
+      <ul className="nav-menu">
+        <li onClick={()=>{setMenu("shop")}}> <Link style={{ textDecoration: 'none' }} to='/'>Home</Link> {menu==="shop"?<hr/>:<></>} </li>
+        <li onClick={()=>{setMenu("mens")}}> <Link style={{ textDecoration: 'none' }}  to='/mens'>Men's</Link> {menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none' }}  to='/womens'>Women's</Link> {menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link  style={{ textDecoration: 'none' }} to='/kids'>Kid's</Link> {menu==="kids"?<hr/>:<></>}</li>
+      </ul>
+
+      <div className="nav-login-cart">
+
+        <Link to='/cart'><img src={cart} alt="" /> </Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
+        <Link to='/singup'><button>Profile</button></Link>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar;
