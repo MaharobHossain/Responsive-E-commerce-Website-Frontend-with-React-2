e4aeeb31a5fa523a50bd2c5c59609898b05840ce
MaharobHossain <maharobhossain8880@gmail.com>
2024-02-22 13:28:13 +0600

    Offer add

---
+++ src/Components/Offer/Offers.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Offers.css'
+import exclucive_image from '../Assest/Images/shopi.png'
+
+const Offers = () => {
+  return (
+
+    
+<div className='offers clearfix'>
+
+<div className="offers-left">
+<h1>Exclusive</h1>
+<h1>Offers For You</h1>
+<p>ONLY ON BEST SELLERS PRODUCT.</p>
+<button>Cheak Now</button>
+</div>
+
+   <div className="offers-right">
+      <img src={exclucive_image} />
+   </div>
+</div>
+    
+    
+  )
+}
+
+export default Offers;
