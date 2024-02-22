8569148f0a2d53a2a55d44811f7eec5d342527f5
MaharobHossain <maharobhossain8880@gmail.com>
2024-02-22 13:26:52 +0600

    Popular product add

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Shop from './Pages/Shop';
-// import ShopCategory from './Pages/ShopCategory';
-// import Product from './Pages/Product';
-// import LoginSignup from './Pages/LoginSignup';
-// import Cart from './Pages/Cart';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup';
+import Cart from './Pages/Cart';
 
-// import men_banner from './Components/Assetss/banner_mens.png'
-// import women_banner from './Components/Assetss/banner_women.png'
-// import kid_banner from './Components/Assetss/banner_kids.png'
-// import Login from './Pages/Login';
+import men_banner from './Components/Assetss/banner_mens.png'
+import women_banner from './Components/Assetss/banner_women.png'
+import kid_banner from './Components/Assetss/banner_kids.png'
+import Login from './Pages/Login';
 import Footer from './Components/Footer/Footer';
 
 function App() {
@@ -21,7 +21,7 @@ function App() {
     <Navbar/>
     <Routes>
     
-    {/* <Route exact path='/' element={<Shop/>} />
+    <Route exact path='/' element={<Shop/>} />
     <Route exact path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
     <Route exact path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
     <Route exact path="/kids" element={<ShopCategory banner={kid_banner} category="kid"/>} />
@@ -30,7 +30,7 @@ function App() {
     </Route>
     <Route exact path='/cart'  element={<Cart/>} />
     <Route exact path='/login'  element={<Login/>} />
-    <Route exact path='/singup'  element={<LoginSignup/>} /> */}
+    <Route exact path='/singup'  element={<LoginSignup/>} />
 
 
     </Routes>
