a36bcea926c286a86ed7b46faafa0bb58c51546a
MaharobHossain <maharobhossain8880@gmail.com>
2024-02-22 14:03:05 +0600

    Home page add

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular';
+import Offers from '../Components/Offer/Offers';
+import NewCollections from '../Components/NewCollections/NewCollections';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
+import './CSS/Shop.css'
+
+
+
+
+
+const Shop = () => {
+  return (
+
+    <div className='row '>
+  
+      <Hero/>
+      <Popular/>
+      <Offers/>
+     <NewCollections/>
+     <NewsLetter/>
+      
+       
+
+    
+    </div>
+  )
+}
+
+export default Shop;
