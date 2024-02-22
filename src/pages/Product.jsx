ea5dd98bce1d72ae9f79d0ea1c969d252c2e8798
MaharobHossain <maharobhossain8880@gmail.com>
2024-02-22 14:01:42 +0600

    product add

---
+++ src/pages/Product.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import all_product from './../Components/Assetss/all_product';
+import { ShopContext } from './../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrums from '../Components/Breadcrums/Breadcrums';
+import ProductDetails from '../Components/ProductDetails/ProductDetails';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from './../Components/RelatedProducts/RelatedProducts';
+
+
+const Product = () => {
+
+  const {all_product} = useContext(ShopContext);
+  const {ProductId} = useParams();
+  const product =all_product.find((e)=> e.id ===  Number(ProductId));
+  return (
+    <div >
+      <Breadcrums product={product} />
+      <ProductDetails product={product} />
+      <DescriptionBox/>
+      <RelatedProducts/>
+    </div>
+  )
+}
+
+export default Product;
